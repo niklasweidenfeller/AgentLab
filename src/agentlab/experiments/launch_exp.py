0328bb197cc62567b8b70ede08abbd9c679c7f6d
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2025-02-03 15:15:59 +0100

    enable connecting against pre-started ray cluster

--- src/agentlab/experiments/launch_exp.py
+++ src/agentlab/experiments/launch_exp.py
@@ -82,7 +82,8 @@ def run_experiments(
         elif parallel_backend == "ray":
             from agentlab.experiments.graph_execution_ray import execute_task_graph, ray
 
-            ray.init(num_cpus=n_jobs)
+            ray.init()
+            # ray.init(num_cpus=n_jobs)
             try:
                 execute_task_graph(exp_args_list, avg_step_timeout=avg_step_timeout)
             finally:
